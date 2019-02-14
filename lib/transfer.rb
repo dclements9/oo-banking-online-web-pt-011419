370ccb81891ba114f71ce7590371a7f2d3a3eeec
Dylan Clements <dclements@protonmail.com>
2019-02-14 22:02:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,6 +27,7 @@ class Transfer
     if valid? && @status == "pending" && @receiver.balance > @amount
       @sender.balance += @amount
       @receiver.balance -= @amount
+      binding.pry
       @status = "reversed"
     else
       @status = "rejected"
